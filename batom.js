74ba271ffd58d5ebd0570bea82a6056b2451b7cd
Lívia <livia.paes8@gmail.com>
2023-06-27 23:10:23 -0300

    first commit

---
+++ batom.js
@@ -0,0 +1,11 @@
+alert ('Preparados para o Lançamento mais esperado do ano?!')
+    Window.document.writeIn ('Olá mundo');
+    document.write ("<br>")
+
+    window.document.writeln(window.document.charset);
+    document.write("<br>")
+    window.document.writeln(window.navigator.appName);
+    document.write("<br>")
+    window.document.writeln(window.document.URL);
+    document.write("<br>")
+    document.write("<br>")
